89f82a39037b1c465c133ea99bfb6c97a025c551
Cesar Bretana Gonzalez <bretanac@gmail.com>
2016-10-15 09:55:42 -0400

    Initial commit

---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ["title", "body"];
+
+    public function comments() {
+    		return $this->hasMany('App\Comment');
+    }
+
+    public function user() {
+    		return $this->belongsTo('App\User');
+    }
+
+    public static function boot() {
+    		parent::boot();
+    		Post::deleting(function ($item) {
+    			$item->comments->delete();
+    			$item->save();
+    		});
+    }
+}
